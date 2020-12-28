5d43930c13f6466b93054784a89155610cc63e52
Arthur Penteado <ascpenteado@gmail.com>
2020-12-28 13:34:29 -0300

    add main route with rss-parser functionality

--- backend/rss-parser.js
+++ backend/rss-parser.js
@@ -1,13 +1,11 @@
 const RSSParser = require("rss-parser");
 const parser = new RSSParser();
 
-export const handleRss = {
-  parseFeed: async () => {
-    const feed = await parser.parseURL("https://www.reddit.com/.rss");
-    console.log(feed.title);
-
-    feed.items.forEach((item) => {
-      console.log(item.title + ":" + item.link);
-    });
+const handleRss = {
+  parseFeed: async (feedUrl) => {
+    const feed = await parser.parseURL(feedUrl);
+    return feed.items;
   },
 };
+
+module.exports = handleRss;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const handleRss = require("./rss-parser");
 
 const PORT = 5000;
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const rssUrl = req.body.url;
+  if (rssUrl) {
+    const feed = await handleRss.parseFeed(rssUrl);
+    return res.json(feed);
+  } else {
+    return res.status(400).json({ message: "URL can't be empty" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

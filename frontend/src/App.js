9d05b032406e68864a3aa58fa99515e4c3211485
Arthur Penteado <ascpenteado@gmail.com>
2020-12-28 15:19:08 -0300

    created basic view for a given feed url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [feedUrl, setFeedUrl] = useState("");
+  const [feed, setFeed] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    getFeed(feedUrl);
+  };
+
+  const getFeed = async (feedUrl) => {
+    const response = await axios.post("http://localhost:5000", { url: feedUrl });
+    setFeed(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>RSS Reader</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="rssUrl"
+          id="rssUrl"
+          placeholder="https://www.example.com/.rss"
+          onChange={(e) => setFeedUrl(e.target.value)}
+        />
+        <br />
+        <br />
+        <button type="submit">Get content</button>
+      </form>
+      <h3>Check some of these cool content:</h3>
+      <ul>
+        <li>
+          <button onClick={() => getFeed("https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss")}>
+            Nasa Image of the Day
+          </button>
+        </li>
+        <li>
+          <button onClick={() => getFeed("https://css-tricks.com/feed/")}>CSS Tricks</button>
+        </li>
+      </ul>
+      <main id="feed">
+        <section>
+          {feed.map((item) => {
+            return (
+              <div key={item.guid}>
+                <img src={item.enclosure ? item.enclosure.url : ""} />
+                <h2>{item.title}</h2>
+                <small>{item.isoDate}</small>
+                <p>{item.contentSnippet}</p>
+                <button>View Article</button>
+                <a target="_blank" rel="noreferrer" href={item.link}>
+                  <button>Go to article</button>
+                </a>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </div>
   );
 }

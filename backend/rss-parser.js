f485241c7c13cf960ae4f77f75cba77a285c7f9b
Arthur Penteado <ascpenteado@gmail.com>
2020-12-30 00:47:01 -0300

    add itemDate to items object, filters items without content snippet and sorts by date

--- backend/rss-parser.js
+++ backend/rss-parser.js
@@ -1,10 +1,28 @@
 const RSSParser = require("rss-parser");
 const parser = new RSSParser();
+const { DateTime } = require("luxon");
 
 const handleRss = {
-  parseFeed: async (feedUrl) => {
+  getFeedItems: async (feedUrl) => {
     const feed = await parser.parseURL(feedUrl);
-    return feed.items;
+    const items = feed.items;
+    const feedItems = items
+      .filter((item) => {
+        if (!item.contentSnippet) {
+          return false;
+        }
+        return true;
+      })
+      .map((item) => {
+        const dt = DateTime.fromISO(item.isoDate);
+        let itemDate = `${dt.c.month}/${dt.c.day}/${dt.c.year}`;
+
+        return { ...item, itemDate };
+      })
+      .sort((a, b) => {
+        DateTime.fromISO(a.isoDate) - DateTime.fromISO(b.isoDate);
+      });
+    return feedItems;
   },
 };
 

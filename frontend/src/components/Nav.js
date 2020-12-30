bce1abe7b781268709bdefefa75d4221bb9e0604
Arthur Penteado <ascpenteado@gmail.com>
2020-12-30 02:55:42 -0300

    add link to the logo so user can refresh page

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,7 +3,9 @@ import React from "react";
 function Nav({ handleSubmit, setFeedUrl }) {
   return (
     <nav className="bg-blue-500 text-gray-800 p-4 md:flex justify-center items-center">
-      <h1 className="text-2xl font-bold text-white text-center">RSS Reader</h1>
+      <h1 className="text-2xl font-bold text-white text-center">
+        <a href="/">RSS Reader</a>
+      </h1>
       <form onSubmit={handleSubmit} className="text-center">
         <input
           className="p-2 rounded-md mt-4 w-full md:mt-0 md:w-auto md:mx-4"
